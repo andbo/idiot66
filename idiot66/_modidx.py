d602fd0fcd4b75d65c585b604665378ecc8b3525
Anders Borchersen <anders.borchersen@lego.com>
2023-02-27 20:10:15 +0100

    fast api first take

--- idiot66/_modidx.py
+++ idiot66/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://andbo.github.io',
                 'git_url': 'https://github.com/andbo/idiot66',
                 'lib_path': 'idiot66'},
-  'syms': {'idiot66.core': {'idiot66.core.foo': ('core.html#foo', 'idiot66/core.py')}}}
+  'syms': { 'idiot66.core': {'idiot66.core.foo': ('core.html#foo', 'idiot66/core.py')},
+            'idiot66.main': {'idiot66.main.root': ('main.html#root', 'idiot66/main.py')},
+            'idiot66.routers.aroute': { 'idiot66.routers.aroute.aroute': ('routers/aroute.html#aroute', 'idiot66/routers/aroute.py'),
+                                        'idiot66.routers.aroute.second': ('routers/aroute.html#second', 'idiot66/routers/aroute.py')}}}
